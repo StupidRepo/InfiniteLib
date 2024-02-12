3ee1e890e5e5ae947d1bfec08db4a342b1e1e594
Bradlee Barnes <69256931+StupidRepo@users.noreply.github.com>
2024-02-12 17:22:39 +0000

    Update infhax.js

--- infhax.js
+++ infhax.js
@@ -4,7 +4,7 @@ if(typeof(gameCode) === "undefined") {
 	console.error("Couldn't get game code. DON'T use the script for now; wait for it to be updated!")
 }
 
-var addNewElement = (name, emoji, disc=false) => {
+var addNewElement = (name, emoji, firstDiscovery=false) => {
 	gameCode.elements.push({
 		emoji: emoji,
 		text: name,
