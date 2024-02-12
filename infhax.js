7653cf16efa4991efd8347e6a8b933105d015804
Bradlee Barnes <69256931+StupidRepo@users.noreply.github.com>
2024-02-12 17:22:09 +0000

    Update infhax.js

--- infhax.js
+++ infhax.js
@@ -1,7 +1,7 @@
 var gameCode = $nuxt.$children.find(l => {return l._uid == "6"}).$children.find(l => {return l._uid == "7"}).$children.find(l => {return l._uid == "9"})
 
 if(typeof(gameCode) === "undefined") {
-	console.error("Couldn't get game code. Please wait for the script to be updated!")
+	console.error("Couldn't get game code. DON'T use the script for now; wait for it to be updated!")
 }
 
 var addNewElement = (name, emoji, disc=false) => {
